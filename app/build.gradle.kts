14635f2b7ad4db4b85c2ccb9e872d66efa029ae7
Vitaliy <abramchyk.hia@gmail.com>
2024-04-28 14:34:46 +0700

    4v

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     alias(libs.plugins.androidApplication)
+    id("com.google.gms.google-services")
+
 }
 
 android {
@@ -34,10 +36,14 @@ dependencies {
 
     implementation(libs.appcompat)
     implementation(libs.material)
+
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
+    implementation(libs.firebase.database)
+    implementation(libs.firebase.firestore)
+    implementation(libs.firebase.storage)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/example/fime_app/OngoingActivity.java
+++ app/src/main/java/com/example/fime_app/OngoingActivity.java
@@ -1,48 +1,83 @@
 package com.example.fime_app;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.content.Intent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.SimpleCursorAdapter;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.fime_app.databinding.OngoingActivityBinding;
+import com.google.android.gms.auth.api.signin.internal.Storage;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.Firebase;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import org.w3c.dom.Text;
+
+import java.io.File;
+import java.io.IOException;
 
 public class OngoingActivity extends AppCompatActivity {
+
+    OngoingActivityBinding binding;
+    StorageReference storageReference;
+    ProgressDialog progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ongoing_activity);
+        binding = OngoingActivityBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        ImageButton ImageButton = findViewById(R.id.scrllviewbox);
 
         // Initialize and assign variable
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
-
         // Set Home selected
         bottomNavigationView.setSelectedItemId(R.id.Ongoing);
-
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-                switch(item.getItemId())
-                {
+                switch (item.getItemId()) {
                     case R.id.Setting:
                         startActivity(new Intent(getApplicationContext(), Setting_tips.class));
-                        overridePendingTransition(0,0);
-                        return  true;
+                        overridePendingTransition(0, 0);
+                        return true;
                     case R.id.History:
-                        startActivity(new Intent(getApplicationContext(),HistoryActivity.class));
-                        overridePendingTransition(0,0);
+                        startActivity(new Intent(getApplicationContext(), HistoryActivity.class));
+                        overridePendingTransition(0, 0);
                         return true;
                     case R.id.Search:
                         startActivity(new Intent(getApplicationContext(), game_selection.class));
-                        overridePendingTransition(0,0);
-                        return  true;
+                        overridePendingTransition(0, 0);
+                        return true;
                     case R.id.Ongoing:
                         return true;
                 }
                 return false;
             }
         });
-
     }
 }
